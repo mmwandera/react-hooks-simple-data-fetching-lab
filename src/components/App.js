9737305cd67f7e47d87955efce5e9e33bf0ac546
mmwandera <mmwandera74@gmail.com>
2023-12-02 15:05:01 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [posts, setPosts] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const url = "https://dog.ceo/api/breeds/image/random"
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data)
+                setPosts(data)
+                setIsLoaded(true)
+            })
+    }, [])
+
+    if(!isLoaded){
+        return <p>Loading...</p>
+    }
+
+    return (
+        <img src={posts.message} alt = "A Random Dog"/>
+    )
+}
+
+export default App;
