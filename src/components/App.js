9baab988984aeba01db17b6ad32ae6ebf13e1e9e
mmwandera <mmwandera74@gmail.com>
2023-12-02 15:07:20 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,9 @@
+// This lab is missing the App component that index.js is looking for. To pass the tests in this lab:
+// Create an App component from scratch
+// Use the useEffect hook in the App component. Inside the callback for useEffect, send a fetch request to https://dog.ceo/api/breeds/image/random, a free API that returns a random image of a dog.
+// Display a <p> tag with the text of "Loading..." when the component is first rendered
+// After receiving a response from the API, show the dog image in an <img> tag, with the alt attribute set to "A Random Dog".
+
 // create your App component here
 
 import React, { useState, useEffect } from "react";
